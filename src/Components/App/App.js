74fabfb6c67cb27ccd4fb2c33633ab4831a7aea2
Mondara <mondara.t@hotmail.com>
2021-06-21 11:33:06 +1000

    Pass down Playlist to TrackList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,10 +10,17 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {searchResults: [
-      {name: "name1", artist: "artist1", album: "album1", id: 1},
-      {name: "name2", artist: "artist2", album: "album2", id: 2}
-    ]};
+    this.state = {
+      searchResults: [
+        {name: "name1", artist: "artist1", album: "album1", id: 1},
+        {name: "name2", artist: "artist2", album: "album2", id: 2}
+        ],
+      playlistName: 'playListName1',
+      playlistTracks: [
+        {name: "name3", artist: "artist3", album: "album3", id: 3},
+        {name: "name4", artist: "artist4", album: "album4", id: 4}
+      ]      
+    };
   }
 
   render() {
@@ -23,8 +30,8 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults = {this.state.searchResults} />
-          <Playlists />
+          <SearchResults searchResults={this.state.searchResults} />
+          <Playlists playlistName={this.state.playListName} playlistTracks={this.state.playlistTracks}/>
         </div>
       </div>
     </div>
